2970a1ba5874a8adf610e198e391ac006c65f540
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-24 00:50:47 +0900

    init project

---
+++ familing/src/main/java/com/pinu/familing/FamilingApplication.java
@@ -0,0 +1,13 @@
+package com.pinu.familing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilingApplication.class, args);
+	}
+
+}
