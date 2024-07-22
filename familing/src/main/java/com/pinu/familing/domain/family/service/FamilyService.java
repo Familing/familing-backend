c0e5452a48016a4f634fb9d63887f88647e694ce
JinSeoHyun <15roa1121@gmail.com>
2024-07-22 12:37:18 +0900

    refact : 패밀리 코드는 핸들러를 이용해 생성, 패밀리서비스는 유효성 검사

--- familing/src/main/java/com/pinu/familing/domain/family/service/FamilyService.java
+++ familing/src/main/java/com/pinu/familing/domain/family/service/FamilyService.java
@@ -1,11 +1,14 @@
 package com.pinu.familing.domain.family.service;
 
+import com.pinu.familing.domain.family.dto.FamilyCode;
 import com.pinu.familing.domain.family.entity.Family;
+import com.pinu.familing.domain.family.handler.FamilyCodeHandler;
 import com.pinu.familing.domain.family.repositiry.FamilyRepository;
 import com.pinu.familing.domain.user.entity.User;
 import com.pinu.familing.domain.user.repository.UserRepository;
 import com.pinu.familing.global.error.CustomException;
 import com.pinu.familing.global.error.ExceptionCode;
+import org.aspectj.apache.bcel.classfile.Code;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +18,6 @@ import java.security.NoSuchAlgorithmException;
 @Service
 public class FamilyService {
 
-    private static final char[] BASE_62_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
     private final UserRepository userRepository;
     private final FamilyRepository familyRepository;
 
@@ -26,40 +28,31 @@ public class FamilyService {
 
     //가족 만들기
     @Transactional
-    public String createFamily(String userName){
-        String code = createCode(userName);
-        String validCode = validFamilyCode(code);
-        //준형님 제가 요즘 도메인에 로직을 담는게 좋다는 이야기를 들었어요. 저도 그렇게 생각하는데.. 흠
-        Family savefamily = familyRepository.save(new Family(validCode));
+    public String registerNewFamily(String userName,String familyName){
+        String validCode = validFamilyCode(FamilyCodeHandler.createCode(userName));
+
+        Family family = new Family(familyName,validCode);
+        Family savefamily = familyRepository.save(family);
+
         return savefamily.getCode();
     }
 
+    @Transactional
+    public void addFamilyToUser(String userName, String code) {
+        User user = userRepository.findByUsername(userName);
 
-    public String validFamilyCode(String code) {
+        Family family = familyRepository.findByCode(code)
+                .orElseThrow(()-> new CustomException(ExceptionCode.INVALID_CODE));
+
+        //내부에 예외 처리 부분 넣어놨습니다.
+        user.registerFamily(family);
+    }
+
+    private String validFamilyCode(String code) {
         if (familyRepository.existsByCode(code)) {
             throw new CustomException(ExceptionCode.INVALID_CODE);
         }
         return code;
-    }
-
-
-    private String createCode(String name) {
-        StringBuilder sb = new StringBuilder();
-
-        // SHA-256 해시 함수를 사용하여 입력값을 해시
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new CustomException(ExceptionCode.FAILED_CODE_GENERATION);
-        }
-        byte[] hash = md.digest(name.getBytes());
-
-        // 해시 값을 Base62로 인코딩
-        for (byte b : hash) {
-            sb.append(BASE_62_CHARS[((b & 0xFF) % BASE_62_CHARS.length)]);
-        }
-        return sb.toString();
     }
 
 }
