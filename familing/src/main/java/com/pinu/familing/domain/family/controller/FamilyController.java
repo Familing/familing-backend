2ed33ac0a8740ca699e8af27e9132476f058efbd
JinSeoHyun <15roa1121@gmail.com>
2024-07-22 12:41:19 +0900

    refactor : 패밀리 컨트롤러 로직 간결하게 변경

--- familing/src/main/java/com/pinu/familing/domain/family/controller/FamilyController.java
+++ familing/src/main/java/com/pinu/familing/domain/family/controller/FamilyController.java
@@ -1,6 +1,7 @@
 package com.pinu.familing.domain.family.controller;
 
 import com.pinu.familing.domain.family.dto.FamilyCode;
+import com.pinu.familing.domain.family.dto.FamilyName;
 import com.pinu.familing.domain.family.service.FamilyService;
 import com.pinu.familing.domain.user.service.UserService;
 import com.pinu.familing.global.oauth.dto.CustomOAuth2User;
@@ -14,25 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class FamilyController {
 
     private final FamilyService familyService;
-    private final UserService userService;
-    public FamilyController(FamilyService familyService, UserService userService) {
+    public FamilyController(FamilyService familyService) {
         this.familyService = familyService;
-        this.userService = userService;
     }
-
-    /*
-     * >  뭔가 제 생각! 가족 유무를 체크 하고 없으면 이 단계를 거치게끔 하고 싶은데요!..!!
-     * >  준형님이 일단 구현을 해보라고 햇으니까 해볼게용
-     *
-     * 흐름
-     * 근데 이 부분 프론트에서 요청을 보낼 것 같네요
-     *  - 유저가 패밀리에 등록되어있는 경우
-     *  - 유저가 패밀리가 없는 경우
-     *
-     * 이 검증도 하나의 메서드에서 할까요?
-     *
-     * 아니면 이건 따로 검증
-     */
 
     /**
      * <가족 생성 로직>
@@ -42,9 +27,9 @@ public class FamilyController {
      *
      */
     @PostMapping("/family")
-    public ResponseEntity<?> registerFamily(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
-        String code = familyService.createFamily(customOAuth2User.getName());
-        userService.addFamilyToUser(customOAuth2User.getName(),code);
+    public ResponseEntity<?> createFamily(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, FamilyName familyName) {
+        String code = familyService.registerNewFamily(customOAuth2User.getName(), familyName.name());
+        familyService.addFamilyToUser(customOAuth2User.getName(),code);
         return ResponseEntity.ok("가족 생성과 추가 성공");
     }
 
@@ -56,8 +41,7 @@ public class FamilyController {
      */
     @PutMapping("/family")
     public ResponseEntity<?> registerFamily(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, FamilyCode familyCode) {
-        String validCode = familyService.validFamilyCode(familyCode.code());
-        userService.addFamilyToUser(customOAuth2User.getName(),validCode);
+        familyService.addFamilyToUser(customOAuth2User.getName(),familyCode.code());
         return ResponseEntity.ok("successfully registered family");
     }
 
