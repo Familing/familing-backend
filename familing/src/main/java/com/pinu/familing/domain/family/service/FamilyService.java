637dd5d8271268a68c97e3dde832b214d67f5738
JunHyeong-99 <sam1616@naver.com>
2024-07-25 16:20:30 +0900

    chore: git ignore에 .idea폴더 추가

--- familing/src/main/java/com/pinu/familing/domain/family/service/FamilyService.java
+++ familing/src/main/java/com/pinu/familing/domain/family/service/FamilyService.java
@@ -1,20 +1,18 @@
 package com.pinu.familing.domain.family.service;
 
-import com.pinu.familing.domain.family.dto.FamilyCode;
+import com.pinu.familing.domain.chat.service.ChatService;
 import com.pinu.familing.domain.family.entity.Family;
 import com.pinu.familing.domain.family.handler.FamilyCodeHandler;
 import com.pinu.familing.domain.family.repositiry.FamilyRepository;
 import com.pinu.familing.domain.user.entity.User;
 import com.pinu.familing.domain.user.repository.UserRepository;
 import com.pinu.familing.global.error.CustomException;
-import com.pinu.familing.global.error.ExceptionCode;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.apache.bcel.classfile.Code;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import static com.pinu.familing.global.error.ExceptionCode.INVALID_CODE;
+import static com.pinu.familing.global.error.ExceptionCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -22,33 +20,42 @@ public class FamilyService {
 
     private final UserRepository userRepository;
     private final FamilyRepository familyRepository;
+    private final ChatService chatService;
 
 
     //가족 만들기
     @Transactional
-    public String registerNewFamily(String userName,String familyName){
-        String validCode = validFamilyCode(FamilyCodeHandler.createCode(userName));
+    public String registerNewFamily(String username,String familyName){
+        String validCode = validFamilyCode(FamilyCodeHandler.createCode(username));
 
         Family family = new Family(familyName,validCode);
         Family savefamily = familyRepository.save(family);
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        // 가족 등록할 때 가족 채팅방 가동 생성
+        chatService.makeChatRoom(user, validCode);
 
         return savefamily.getCode();
     }
 
     @Transactional
-    public void addFamilyToUser(String userName, String code) {
-        User user = userRepository.findByUsername(userName);
+    public void addFamilyToUser(String username, String code) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
         Family family = familyRepository.findByCode(code)
-                .orElseThrow(()-> new CustomException(ExceptionCode.INVALID_CODE));
+                .orElseThrow(()-> new CustomException(INVALID_CODE));
 
         //내부에 예외 처리 부분 넣어놨습니다.
         user.registerFamily(family);
+
     }
 
     private String validFamilyCode(String code) {
         if (familyRepository.existsByCode(code)) {
-            throw new CustomException(ExceptionCode.INVALID_CODE);
+            throw new CustomException(INVALID_CODE);
         }
         return code;
     }
