13185eba30f2c550c21a268e03c6a40b85ba71e4
JunHyeong-99 <sam1616@naver.com>
2024-07-25 15:40:35 +0900

    feat: Chatting mongodb 구현

---
+++ familing/src/main/java/com/pinu/familing/domain/chat/Chatting.java
@@ -0,0 +1,28 @@
+package com.pinu.familing.domain.chat;
+
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "chatting")
+@Getter
+@ToString
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+// MongoDB Chatting 모델
+public class Chatting {
+
+    @Id
+    private String id;
+    private Long chatRoomNo;
+    private Long senderNo;
+    private String senderName;
+    private String contentType;
+    private String content;
+    private LocalDateTime sendDate;
+
+}
