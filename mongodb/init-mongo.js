e18eab14cf527fdb9fc63b5455fb4b8ad57c9496
JunHyeong-99 <sam1616@naver.com>
2024-07-31 21:42:46 +0900

    fix(init-mongo.js): mongo데이터 베이스  testdb 초기화 js파일 수정

--- mongodb/init-mongo.js
+++ mongodb/init-mongo.js
@@ -10,3 +10,15 @@ db.createUser({
         db: 'familing'
     }]
 });
+
+
+db = db.getSiblingDB('testdb'); // testdb 데이터베이스로 전환
+
+db.createUser({
+    user: 'root',
+    pwd: '1234',
+    roles: [{
+        role: 'readWrite',
+        db: 'testdb'
+    }]
+});
