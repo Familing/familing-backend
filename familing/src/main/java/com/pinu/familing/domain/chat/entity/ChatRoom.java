3415cab83717ca374967e3f6a10e16de5a6b11f5
JunHyeong-99 <sam1616@naver.com>
2024-07-31 16:13:03 +0900

    fix: validCode에 NotNull추가

--- familing/src/main/java/com/pinu/familing/domain/chat/entity/ChatRoom.java
+++ familing/src/main/java/com/pinu/familing/domain/chat/entity/ChatRoom.java
@@ -3,6 +3,7 @@ package com.pinu.familing.domain.chat.entity;
 import com.pinu.familing.domain.BaseEntity;
 import com.pinu.familing.domain.user.entity.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class ChatRoom extends BaseEntity {
     @Column(name = "chat_room_id")
     private Long id;
 
+    @NotNull(message = "채팅방 생성시 가족코드는 필수입니다.")
     private String validCode;
 
     @OneToMany(mappedBy = "chatRoom")
