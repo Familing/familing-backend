f821974e2ac99cd678d855003e207c5712a12b76
JinSeoHyun <15roa1121@gmail.com>
2024-07-31 19:43:09 +0900

    feat : 유저 프로필 업데이트 기능 구현

--- familing/src/main/java/com/pinu/familing/domain/user/controller/UserController.java
+++ familing/src/main/java/com/pinu/familing/domain/user/controller/UserController.java
@@ -1,16 +1,48 @@
 package com.pinu.familing.domain.user.controller;
 
+import com.pinu.familing.domain.user.dto.ImageUrl;
+import com.pinu.familing.domain.user.dto.Nickname;
+import com.pinu.familing.domain.user.dto.Realname;
+import com.pinu.familing.domain.user.dto.UserResponse;
+import com.pinu.familing.domain.user.service.UserService;
 import com.pinu.familing.global.oauth.dto.CustomOAuth2User;
+import com.pinu.familing.global.util.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class UserController {
+
+    private final UserService userService;
+
     @GetMapping("/user")
-    public String testUser(CustomOAuth2User principal) {
-        return principal.toString();
+    public ApiUtils.ApiResult<UserResponse> giveUserInformation(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        UserResponse userResponse = userService.giveUserInformation(customOAuth2User);
+        return ApiUtils.success(HttpStatus.OK, userResponse);
+    }
+
+    //닉네임 변경
+    @PostMapping("/user/nickname")
+    public ApiUtils.ApiResult<?> changeNickname(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody Nickname nickname) {
+        userService.changeNickname(customOAuth2User, nickname);
+        return ApiUtils.success(HttpStatus.OK,"Successful nickname changed");
+    }
+
+    //진짜 이름 변경
+    @PostMapping("/user/realname")
+    public ApiUtils.ApiResult<?> changeRealname(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody Realname realname) {
+        userService.changeRealname(customOAuth2User, realname);
+        return ApiUtils.success(HttpStatus.OK,"Successful realname changed");
+    }
+
+    //프로필 변경
+    @PostMapping("/user/imageurl")
+    public ApiUtils.ApiResult<?> changeImageUrl(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody ImageUrl imageUrl) {
+        userService.changeImageUrl(customOAuth2User, imageUrl);
+        return ApiUtils.success(HttpStatus.OK,"Successful image url changed");
     }
 }
--- familing/src/main/java/com/pinu/familing/domain/user/service/UserService.java
+++ familing/src/main/java/com/pinu/familing/domain/user/service/UserService.java
@@ -1,9 +1,19 @@
 package com.pinu.familing.domain.user.service;
 
+import com.pinu.familing.domain.family.entity.Family;
 import com.pinu.familing.domain.family.repositiry.FamilyRepository;
+import com.pinu.familing.domain.user.dto.ImageUrl;
+import com.pinu.familing.domain.user.dto.Nickname;
+import com.pinu.familing.domain.user.dto.Realname;
+import com.pinu.familing.domain.user.dto.UserResponse;
+import com.pinu.familing.domain.user.entity.User;
 import com.pinu.familing.domain.user.repository.UserRepository;
+import com.pinu.familing.global.error.CustomException;
+import com.pinu.familing.global.error.ExceptionCode;
+import com.pinu.familing.global.oauth.dto.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +22,41 @@ public class UserService {
     private final UserRepository userRepository;
     private final FamilyRepository familyRepository;
 
+
+    public UserResponse giveUserInformation(CustomOAuth2User customOAuth2User) {
+        User user = userRepository.findByUsername(customOAuth2User.getName());
+        return new UserResponse(user);
+    }
+
+
+    @Transactional
+    public void addFamilyToUser(CustomOAuth2User customOAuth2User, String code) {
+        //유저 찾기
+        User user = userRepository.findByUsername(customOAuth2User.getName());
+
+        Family family = familyRepository.findByCode(code)
+                .orElseThrow(()-> new CustomException(ExceptionCode.INVALID_CODE));
+
+        //내부에 예외 처리 부분 넣어놨습니다.
+        user.registerFamily(family);
+    }
+
+    @Transactional
+    public void changeNickname(CustomOAuth2User customOAuth2User, Nickname nickname) {
+        User user = userRepository.findByUsername(customOAuth2User.getName());
+        user.updateNickname(nickname);
+    }
+
+    @Transactional
+    public void changeRealname(CustomOAuth2User customOAuth2User, Realname realname) {
+        User user = userRepository.findByUsername(customOAuth2User.getName());
+        user.updateRealname(realname);
+    }
+
+    @Transactional
+    public void changeImageUrl(CustomOAuth2User customOAuth2User, ImageUrl imageUrl) {
+        User user = userRepository.findByUsername(customOAuth2User.getName());
+        user.updateImageUrl(imageUrl);
+    }
 
 }
