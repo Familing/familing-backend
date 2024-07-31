a141ebbf7539e767f6abe7093a830746697ec02e
JinSeoHyun <15roa1121@gmail.com>
2024-07-31 13:50:14 +0900

    feat : 구독 타입 만들기

---
+++ familing/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO subscription (name, price, explanation) VALUES ('한달 무료 체험', 0, '한달 무료체험 가능 사실 3주로 기획했는데 어쩌다보니 4주됨.');
+INSERT INTO subscription (name, price, explanation) VALUES ('일반형', 2900, '일반형, 기본 기능이고 상태보기 기능 다양, 애정카드 기능 다양');
+INSERT INTO subscription (name, price, explanation) VALUES ('프리미엄형', 3900, '프리미엄 구독입니다. 모든 기능을 무제한으로 제공합니다.');
