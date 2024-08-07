d92b125e1eaa18e07e883c286a0d1361f7db40da
JinSeoHyun <15roa1121@gmail.com>
2024-08-07 21:08:55 +0900

    refactor(FirebaseCloudMessageService: FCM 메시지 보내는 서비스 로직 수정

--- familing/src/main/java/com/pinu/familing/global/firebase/service/FirebaseCloudMessageService.java
+++ familing/src/main/java/com/pinu/familing/global/firebase/service/FirebaseCloudMessageService.java
@@ -5,43 +5,45 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.pinu.familing.global.firebase.dto.FcmMessage;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
+import com.squareup.okhttp.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpHeaders;
-import com.squareup.okhttp.MediaType;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 
 
 @Component
 @RequiredArgsConstructor
 public class FirebaseCloudMessageService {
 
+
     private final String API_URL = "https://fcm.googleapis.com/v1/projects/familing-def7e/messages:send";
+    private final String YOUR_SERVER_KEY = "42540005161";
+    private static final String SCOPES = "https://www.googleapis.com/auth/firebase.messaging";
+
     private final ObjectMapper objectMapper;
 
     public void sendMessageTo(String targetToken, String title, String body) throws IOException, JsonProcessingException {
 
         String message = makeMessage(targetToken, title, body);
-
         OkHttpClient client = new OkHttpClient();
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"),message);
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody requestBody = RequestBody.create(JSON, message);
+
         Request request = new Request.Builder()
                 .url(API_URL)
                 .post(requestBody)
-                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
-                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .addHeader("Authorization", "Bearer " + getAccessToken())
+                .addHeader("Content-Type", "application/json; UTF-8")
                 .build();
 
         Response response = client.newCall(request).execute();
 
-        System.out.println(response.body().string());
+        System.out.println("Response Code: " + response.code());
+        System.out.println("Response Body: " + response.body().string());
+
     }
 
     private String makeMessage(String targetToken, String title, String body) throws JsonParseException, JsonProcessingException {
@@ -58,15 +60,28 @@ public class FirebaseCloudMessageService {
         return objectMapper.writeValueAsString(fcmMessage);
     }
 
-    private String getAccessToken() throws IOException {
-        String firebaseConfigPath = "firebase/firebase_service_key.json";
+    private static String getAccessToken() throws IOException {
+//        GoogleCredentials googleCredentials = GoogleCredentials
+//                .fromStream(new ClassPathResource("firebase/firebase_service_key.json").getInputStream())
+//                .createScoped(Arrays.asList(SCOPES));
+//        googleCredentials.refreshAccessToken();
+//        return googleCredentials.getAccessToken().getTokenValue();
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource("firebase/firebase_service_key.json").getInputStream())
+                    .createScoped(Arrays.asList(SCOPES));
 
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
-                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+            googleCredentials.refreshIfExpired();
+            if (googleCredentials.getAccessToken() == null) {
+                googleCredentials.refresh();
 
-        googleCredentials.refreshIfExpired();
-        return googleCredentials.getAccessToken().getTokenValue();
+            }
+            System.out.println("googleCredentials.getAccessToken().getTokenValue() = " + googleCredentials.getAccessToken().getTokenValue());
+            return googleCredentials.getAccessToken().getTokenValue();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to obtain access token", e);
+        }
     }
-}
 
+}
