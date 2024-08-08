e6ef45b3991bac4665efe8f077a59d80fb9edc77
JinSeoHyun <15roa1121@gmail.com>
2024-08-08 13:13:31 +0900

    refactor(SnapshotUser) : 엔티티 명 수정

--- familing/src/main/java/com/pinu/familing/domain/snapshot/entity/SnapshotUser.java
+++ familing/src/main/java/com/pinu/familing/domain/snapshot/entity/SnapshotUser.java
@@ -3,21 +3,22 @@ package com.pinu.familing.domain.snapshot.entity;
 import com.pinu.familing.domain.BaseEntity;
 import com.pinu.familing.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class SnapshotImage  extends BaseEntity {
+public class SnapshotUser extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "snapshot_group_id",nullable = false)
-    private SnapshotGroup snapshotGroup;
+    @JoinColumn(name = "snapshot_id",nullable = false)
+    private Snapshot snapshot;
 
     @ManyToOne
     @JoinColumn(name = "user_id",nullable = false)
@@ -27,4 +28,10 @@ public class SnapshotImage  extends BaseEntity {
     private String imageUrl;
 
 
+    @Builder
+    public SnapshotUser(Snapshot snapshot, User user, String imageUrl) {
+        this.snapshot = snapshot;
+        this.user = user;
+        this.imageUrl = imageUrl;
+    }
 }
