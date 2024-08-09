6cc316f121aaa01e2a0509e6e7aaf2dea322da5f
JinSeoHyun <15roa1121@gmail.com>
2024-08-09 19:17:25 +0900

    feat: 스냅샷 유저 가져오는 쿼리 메서드 추가

--- familing/src/main/java/com/pinu/familing/domain/snapshot/repository/SnapshotUserRepository.java
+++ familing/src/main/java/com/pinu/familing/domain/snapshot/repository/SnapshotUserRepository.java
@@ -1,8 +1,14 @@
 package com.pinu.familing.domain.snapshot.repository;
 
+import com.pinu.familing.domain.snapshot.entity.Snapshot;
 import com.pinu.familing.domain.snapshot.entity.SnapshotUser;
+import com.pinu.familing.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 
 public interface SnapshotUserRepository extends JpaRepository<SnapshotUser, Long> {
+    Optional<SnapshotUser> findByUserAndDate(User user, LocalDate day);
 }
