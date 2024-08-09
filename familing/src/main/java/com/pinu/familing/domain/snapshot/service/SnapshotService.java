c09ce590a4b76ba00d3a6ee9297b8e80e7238036
JinSeoHyun <15roa1121@gmail.com>
2024-08-09 19:22:33 +0900

    refactor(SnapshotService) : getUser메서드에 가족 등록 안된 경우 예외 발생시키는 로직 추가와 메서드명 을 더 구체적으로 변경

--- familing/src/main/java/com/pinu/familing/domain/snapshot/service/SnapshotService.java
+++ familing/src/main/java/com/pinu/familing/domain/snapshot/service/SnapshotService.java
@@ -4,7 +4,6 @@ import com.pinu.familing.domain.snapshot.dto.SnapshotImageRequest;
 import com.pinu.familing.domain.snapshot.dto.SnapshotResponse;
 import com.pinu.familing.domain.snapshot.entity.Snapshot;
 import com.pinu.familing.domain.snapshot.entity.SnapshotUser;
-import com.pinu.familing.domain.snapshot.entity.SnapshotTitle;
 import com.pinu.familing.domain.snapshot.repository.SnapshotRepository;
 import com.pinu.familing.domain.snapshot.repository.SnapshotUserRepository;
 import com.pinu.familing.domain.user.entity.User;
@@ -12,9 +11,12 @@ import com.pinu.familing.domain.user.repository.UserRepository;
 import com.pinu.familing.global.error.CustomException;
 import com.pinu.familing.global.error.ExceptionCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,59 +28,56 @@ public class SnapshotService {
     private final TitleService titleService;
 
 
-
     //스냅샷에 이미지 등록하기
     public void registerSnapshotImage(LocalDate day, String name, SnapshotImageRequest snapshotImageRequest) {
-        User user = getUser(name);
-        if (user.getFamily() == null) {
-            throw new CustomException(ExceptionCode.FAMILY_NOT_FOUND);
-        }
-        Snapshot snapshot = snapshotRepository.findByFamilyAndDate(user.getFamily(), day)
+        User user = getUserWithFamily(name);
+
+        SnapshotUser snapshotUser = snapshotUserRepository.findByUserAndDate(user, day)
                 .orElseThrow(() -> new CustomException(ExceptionCode.SNAPSHOT_NOT_FOUND));
 
-        snapshotUserRepository.save(SnapshotUser.builder()
-                        .snapshot(snapshot)
-                        .user(user)
-                        .imageUrl(snapshotImageRequest.imageUrl())
-                        .build());
-
+        snapshotUser.updateImage(snapshotImageRequest.imageUrl());
     }
 
-    //패밀리스냅샷만들기
+    //스냅샷 전체 그룹 만들기 (개인별 스냅샷도 이때 함께 생성됨.)
     public void createSnapshotGroup(LocalDate day, String name) {
-        User user = getUser(name);
-        if (user.getFamily() == null) {
-            throw new CustomException(ExceptionCode.FAMILY_NOT_FOUND);
-        }
-        //이미 존재하는 경우에 대해서는 동작하지 않음
-        if (snapshotRepository.existsByFamilyAndDate(user.getFamily(),day)){
-            return;
-        }
+        User user = getUserWithFamily(name);
 
-        //해당 날짜에 맞는 주제를 가져오는 로직
-        SnapshotTitle snapshotTitle = titleService.getTitle(day);
+        Snapshot savedSnapshot = snapshotRepository.save(
+                new Snapshot(user.getFamily(), titleService.getTitle(day), day));
 
-        snapshotRepository.save(new Snapshot(user.getFamily(), snapshotTitle, day));
+        List<User> familyMembers = userRepository.findAllByFamily(user.getFamily());
+
+        familyMembers.forEach((familyMember) -> {
+            SnapshotUser snapshotUser = new SnapshotUser(savedSnapshot, familyMember, day);
+            snapshotUserRepository.save(snapshotUser);
+        });
     }
 
-    public SnapshotResponse offerSnapshot(LocalDate day, String name) {
-        User user = getUser(name);
-        if (user.getFamily() == null) {
-            throw new CustomException(ExceptionCode.FAMILY_NOT_FOUND);
-        }
-        Snapshot snapshot =  snapshotRepository.findByFamilyAndDate(user.getFamily(), day)
+    //스냅샷 페이지 조회
+    public Page<SnapshotResponse> provideSnapshotPage(LocalDate day, Pageable pageable, String name) {
+        User user = getUserWithFamily(name);
+        return snapshotRepository.findAllByFamilyAndDateBefore(user.getFamily(), day, pageable)
+                .map(SnapshotResponse::new);
+    }
+
+    //특정 날짜 스냅샷 조회
+    public SnapshotResponse provideSnapshot(LocalDate day, String name) {
+        User user = getUserWithFamily(name);
+        Snapshot snapshot = snapshotRepository.findByFamilyAndDate(user.getFamily(), day)
                 .orElseThrow(() -> new CustomException(ExceptionCode.SNAPSHOT_NOT_FOUND));
 
         return new SnapshotResponse(snapshot);
     }
 
-    //유저값가져오기
-    private User getUser(String name) {
-        return userRepository.findByUsername(name)
+    //유저 값 가져오기
+    private User getUserWithFamily(String name) {
+        User user = userRepository.findByUsername(name)
                 .orElseThrow(() -> new CustomException(ExceptionCode.USER_NOT_FOUND));
+        if (user.getFamily() == null) {
+            throw new CustomException(ExceptionCode.FAMILY_NOT_FOUND);
+        }
 
-
-
+        return user;
     }
 
 
