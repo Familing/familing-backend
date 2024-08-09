bc7b6855ee57bb2d0290f4fff58bb7a7278f1e7e
JinSeoHyun <15roa1121@gmail.com>
2024-08-09 19:27:05 +0900

    refactor: 스냅샷 이미지 등록시 유효성 검사 로직 추가

--- familing/src/main/java/com/pinu/familing/domain/snapshot/dto/SnapshotImageRequest.java
+++ familing/src/main/java/com/pinu/familing/domain/snapshot/dto/SnapshotImageRequest.java
@@ -2,7 +2,9 @@ package com.pinu.familing.domain.snapshot.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record SnapshotImageRequest(String imageUrl) {
+public record SnapshotImageRequest(
+        @NotNull String imageUrl) {
 }
