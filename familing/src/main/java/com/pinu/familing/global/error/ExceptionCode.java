99d7d55da0741b20b3ac14b92dcb6038be35d757
JinSeoHyun <15roa1121@gmail.com>
2024-08-14 00:10:26 +0900

    fix: 중복enum 삭제

--- familing/src/main/java/com/pinu/familing/global/error/ExceptionCode.java
+++ familing/src/main/java/com/pinu/familing/global/error/ExceptionCode.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public enum ExceptionCode {
     // 사용자 관련 에러
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
@@ -23,8 +23,7 @@ public enum ExceptionCode {
 
     //가족 코드 생성중
     FAILED_CODE_GENERATION(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
-    ALREADY_HAVE_FAMILY(HttpStatus.BAD_REQUEST, "가족이 있습니다"),
-    FAMILY_NOT_FOUND(HttpStatus.BAD_REQUEST, "가족이 없습니다"),
+    ALREADY_HAVE_FAMILY(HttpStatus.BAD_REQUEST, "가족이 있습니다."),
     INVALID_CODE(HttpStatus.BAD_REQUEST, "코드가 유효하지않습니다."),
 
     //구독 관련
@@ -33,10 +32,7 @@ public enum ExceptionCode {
     //스냅샷 관련
     SNAPSHOT_TITLE_NOT_FOUND(HttpStatus.NOT_FOUND, "주제가 존재하지 않습니다."),
     SNAPSHOT_NOT_FOUND(HttpStatus.NOT_FOUND, "스냅샷이 생성되지 않았습니다."),
-    DAY_BAD_REQUEST(HttpStatus.BAD_REQUEST, "DAY 형식이 잘못되었습니다."),
-
-    ;
-
+    DAY_BAD_REQUEST(HttpStatus.BAD_REQUEST, "DAY 형식이 잘못되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
