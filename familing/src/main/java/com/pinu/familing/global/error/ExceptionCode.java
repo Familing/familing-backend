05a95c3d8b47f11ec0e0891992fcaa652f83fc07
JunHyeong-99 <64734115+JunHyeong-99@users.noreply.github.com>
2024-08-14 01:39:48 +0900

    fix: nodejs삭제

--- familing/src/main/java/com/pinu/familing/global/error/ExceptionCode.java
+++ familing/src/main/java/com/pinu/familing/global/error/ExceptionCode.java
@@ -24,7 +24,6 @@ public enum ExceptionCode {
     //가족 코드 생성중
     FAILED_CODE_GENERATION(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
     ALREADY_HAVE_FAMILY(HttpStatus.BAD_REQUEST, "가족이 있습니다"),
-    FAMILY_NOT_FOUND(HttpStatus.BAD_REQUEST, "가족이 없습니다"),
     INVALID_CODE(HttpStatus.BAD_REQUEST, "코드가 유효하지않습니다."),
 
     //구독 관련
