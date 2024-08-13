e5e37648abda4011261c6e5f4c9ac1e8964af956
JinSeoHyun <15roa1121@gmail.com>
2024-08-13 18:06:20 +0900

    remove : 커스텀 생성자 삭제

--- familing/src/main/java/com/pinu/familing/domain/snapshot/entity/Snapshot.java
+++ familing/src/main/java/com/pinu/familing/domain/snapshot/entity/Snapshot.java
@@ -3,10 +3,7 @@ package com.pinu.familing.domain.snapshot.entity;
 import com.pinu.familing.domain.BaseEntity;
 import com.pinu.familing.domain.family.entity.Family;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,7 +11,9 @@ import java.util.List;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 protected로 설정
+@Builder
+@NoArgsConstructor // 기본 생성자를 protected로 설정
+@AllArgsConstructor
 public class Snapshot extends BaseEntity {
 
     @Id
@@ -35,12 +34,4 @@ public class Snapshot extends BaseEntity {
     @OneToMany(orphanRemoval = true, mappedBy = "snapshot", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<SnapshotImage> snapshotImages = new ArrayList<>();
 
-    // 특정 생성자에만 @Builder 적용
-    @Builder
-    public Snapshot(Family family, SnapshotTitle snapshotTitle, LocalDate date) {
-        this.family = family;
-        this.snapshotTitle = snapshotTitle;
-        this.date = date;
-        this.snapshotImages = new ArrayList<>();
-    }
 }
