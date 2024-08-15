93e84a89492866a52502e52b77e1707df8fcadf7
JinSeoHyun <15roa1121@gmail.com>
2024-08-15 21:21:17 +0900

    feat: 빌더추가

--- familing/src/main/java/com/pinu/familing/domain/lovecard/entity/LovecardLog.java
+++ familing/src/main/java/com/pinu/familing/domain/lovecard/entity/LovecardLog.java
@@ -1,14 +1,16 @@
 package com.pinu.familing.domain.lovecard.entity;
 
+import com.pinu.familing.domain.BaseEntity;
 import com.pinu.familing.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class LovecardLog {
+public class LovecardLog extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,6 +27,10 @@ public class LovecardLog {
     @JoinColumn(name = "sender_id", nullable = false)
     private User sender;
 
-
-
+    @Builder
+    public LovecardLog(Lovecard lovecard, User receiver, User sender) {
+        this.lovecard = lovecard;
+        this.receiver = receiver;
+        this.sender = sender;
+    }
 }
