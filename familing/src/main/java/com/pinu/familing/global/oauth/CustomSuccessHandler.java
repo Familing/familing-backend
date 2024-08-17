db4e7fc7309d9885fb9cbb625c2af353647211c9
JunHyeong-99 <64734115+JunHyeong-99@users.noreply.github.com>
2024-08-17 16:57:22 +0900

    fix: CustomSucessHandler ip주소 환경 변수로 빼기

--- familing/src/main/java/com/pinu/familing/global/oauth/CustomSuccessHandler.java
+++ familing/src/main/java/com/pinu/familing/global/oauth/CustomSuccessHandler.java
@@ -19,8 +19,8 @@ import java.util.Iterator;
 @Component
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    @Value("${server.address}")
-    private String serverIp;
+    @Value("${aws.myip}")
+    private String myIp;
 
     private final JWTUtil jwtUtil;
 
@@ -47,12 +47,12 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         response.addCookie(createCookie("Authorization", token));
         // 로그인 성공 후 Redirect 주소
         if ("ROLE_PENDING_USER".equals(role)) { // Oauth만 인증되고 추가적인 요청 필요
-            response.sendRedirect("localhost:8080:api/v1/user");
+            response.sendRedirect("myapp://auth/login");
         } else if ("ROLE_USER".equals(role)) { // 회원가입 완료
             response.sendRedirect("myapp://callback/register-screen1");
         } else {
             //Role이 없으면 다시 로그인 창으로
-            response.sendRedirect("http://"+serverIp+"/oauth2/authorization/kakao");
+            response.sendRedirect("http://"+myIp+"/oauth2/authorization/kakao");
         }
     }
 
