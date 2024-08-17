ee768e79f7b8d06850a40c4a8dbb7132f2766768
JinSeoHyun <15roa1121@gmail.com>
2024-08-17 19:21:12 +0900

    feat: 상태값반환시 id값도 함께 반환

--- familing/src/main/java/com/pinu/familing/domain/userstatus/dto/UserstatusResponse.java
+++ familing/src/main/java/com/pinu/familing/domain/userstatus/dto/UserstatusResponse.java
@@ -2,9 +2,10 @@ package com.pinu.familing.domain.userstatus.dto;
 
 import com.pinu.familing.domain.userstatus.entity.Userstatus;
 
-public record UserstatusResponse(String text) {
+public record UserstatusResponse(Long id,
+                                 String text) {
 
     public UserstatusResponse(Userstatus userstatus) {
-        this(userstatus.getText());
+        this(userstatus.getId(),userstatus.getText());
     }
 }
