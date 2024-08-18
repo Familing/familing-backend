8bb9f2f4e5ff5a65bc77388d53db05a75b8e30e3
JinSeoHyun <15roa1121@gmail.com>
2024-08-18 10:04:42 +0900

    fix : 응답객체 만듬

--- familing/src/main/java/com/pinu/familing/domain/snapshot/dto/SnapshotResponse.java
+++ familing/src/main/java/com/pinu/familing/domain/snapshot/dto/SnapshotResponse.java
@@ -12,29 +12,68 @@ import java.util.stream.Collectors;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SnapshotResponse(String Title,
                                LocalDate date,
-                               List<UserSnapshot> SnapshotImageList) {
+                               UserSnapshot me,
+                               List<UserSnapshot> family) {
 
-    public SnapshotResponse(Snapshot snapshot) {
+    public SnapshotResponse(String username, Snapshot snapshot) {
         this(
                 snapshot.getSnapshotTitle().getTitle(),
                 snapshot.getDate(),
                 snapshot.getSnapshotImages().stream()
+                        .filter(snapshotImage -> snapshotImage.getUser().getUsername().equals(username))
                         .map(UserSnapshot::new)
-                        .collect(Collectors.toList())
-        );
+                        .toList().get(0),
+                snapshot.getSnapshotImages().stream()
+                        .filter(snapshotImage -> snapshotImage.getUser().getUsername().equals(username))
+                        .map(UserSnapshot::new)
+                        .collect(Collectors.toList()
+                        );
     }
 
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     record UserSnapshot(String username,
-                                   String nickname,
-                                   String userProfile,
-                                   String snapshotImage) {
+                        String nickname,
+                        String userProfile,
+                        String snapshotImage) {
 
         public UserSnapshot(SnapshotImage snapshotImage) {
             this(snapshotImage.getUser().getUsername(), snapshotImage.getUser().getNickname(), snapshotImage.getUser().getImageUrl(), snapshotImage.getImageUrl());
         }
     }
 
+    /*
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record MyFamilyStatusResponse(UserStatusResponse me,
+                                     List<UserStatusResponse> family) {
+
+    public MyFamilyStatusResponse(User user) {
+        this(
+                new UserStatusResponse(user)
+                ,user.getFamily().getUsers().stream()
+                .filter(familyUser -> !familyUser.equals(user)) // user와 동일한 객체는 건너뜀
+                .map(UserStatusResponse::new) // UserResponse로 변환
+                .collect(Collectors.toList()) // List로 변환
+        );
+    }
+
+
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    private record UserStatusResponse(String username,
+                                     String nickname,
+                                     String imageUrl,
+                                     String status) {
+        private UserStatusResponse(User user) {
+            this(
+                    user.getUsername(),
+                    user.getNickname(),
+                    user.getImageUrl(),
+                    user.getStatus().getText()// status가 null인 경우 "EMPTY"로 설정
+            );
+        }
+    }
+}
+     */
 
 }
 
