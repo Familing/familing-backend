4fbce8328add3a191e2ffc14988fc7c2375f2859
JunHyeong-99 <64734115+JunHyeong-99@users.noreply.github.com>
2024-08-28 20:18:31 +0900

    fix: 알림 dto snake로변경

--- familing/src/main/java/com/pinu/familing/domain/alarm/dto/AlarmDto.java
+++ familing/src/main/java/com/pinu/familing/domain/alarm/dto/AlarmDto.java
@@ -1,8 +1,11 @@
 package com.pinu.familing.domain.alarm.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.pinu.familing.domain.alarm.AlarmType;
 import com.pinu.familing.domain.alarm.entity.Alarm;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AlarmDto(
         Long id,
         Boolean isRead,
