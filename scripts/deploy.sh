8bd189e6f9ec2224ac60d0b8b31f577d3ba1845b
JunHyeong-99 <64734115+JunHyeong-99@users.noreply.github.com>
2024-08-31 15:08:04 +0900

    chore: 배포 스크립트 로그 파일에 저장하도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,23 +2,23 @@
 BUILD_JAR=$(ls /home/ubuntu/action/familing/build/libs/familing-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> 현재 시간: $(date)"
+echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
 
-echo "> build 파일명: $JAR_NAME"
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
 
-echo "> build 파일 복사"
+echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
 DEPLOY_PATH=/home/ubuntu/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인"
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PIDS=$(pgrep -f "java -jar /home/ubuntu/action/$JAR_NAME" | head -n 1)
-echo $CURRENT_PIDS
+echo $CURRENT_PIDS >> /home/ubuntu/action/deploy.log
 
 if [ -z "$CURRENT_PIDS" ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
 else
-  echo "> kill -9 $CURRENT_PIDS"
+  echo "> kill -9 $CURRENT_PIDS" >> /home/ubuntu/action/deploy.log
   sudo kill -9 $CURRENT_PIDS
   sleep 5
 fi
